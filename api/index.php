97c2f0d3a86d5d36cfd9592bdb1857ab27527436
Ranyelton Santos <62861522+Ranyelton@users.noreply.github.com>
2024-11-22 19:46:55 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rosca Company</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+        <h3>O caminho único para que a construção de <br> <u>mentes melhores</u> em meio ao novo mundo.</h3>
+    </div>
+</body>
+</html>
